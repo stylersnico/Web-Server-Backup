283253d49d3cf39f1c8dd97e73de3a59d82cc450
Nicolas Simond <stylersnico@gmail.com>
2015-11-09 09:16:26 +0100

    Update allinonebackup.sh

--- allinonebackup.sh
+++ allinonebackup.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
+###############
+###Variables###
+###############
+
+#backup of the WWW folder
+WWWBACKUPDIR="/backup/WWW/"
+
+#backup of the databases
+DBBACKUPDIR="/backup/DBs/"
+DBUSER="root"
+DBPASSWORD="yourrootpasswordofmysqlormariadb"
+
+#Send to external FTP storage
+ROOTFOLDER="/backup/*"
+ARCHIVEPASSWORD="yourawesomepasswordbecauseyesyougiveafuckaboutsecurity"
+ARCHIVEFOLDER="/home/someuser/"
+FTPSERVER="server"
+FTPUSER="user"
+FTPPASS="pass"
+
+
 ##############################
 ###backup of the WWW folder###
 ##############################
 
 #Go to working dir
-cd /backup/WWW/
+cd $WWWBACKUPDIR
 
 #Create a backup of all the /var/www/ dir
 tar -czf `date +%Y%m%d`.www.tar.gz /var/www/
@@ -15,22 +36,17 @@ tar -czf `date +%Y%m%d`.www.tar.gz /var/www/
 ###backup of the databases###
 #############################
 
-#Variables
-USER="root"
-PASSWORD="yourrootpasswordofmysqlormariadb"
-OUTPUT="/backup/DBs"
-
 #Go to working dir
-cd /backup/DBs/
+cd $DBBACKUPDIR
 
 #Extract all databases
-databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+databases=`mysql -u $DBUSER -p$DBPASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 #Backup all databases
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump -u $USER -p$PASSWORD --databases $db > `date +%Y%m%d`.$db.sql
+        mysqldump -u $DBUSER -p$DBPASSWORD --databases $db > `date +%Y%m%d`.$db.sql
     fi
 done
 
@@ -41,17 +57,17 @@ done
 
 #Creating 7zip encrypted archive
 cd /root/
-7za a -y -tzip -pyourawesomepasswordbecauseyesyougiveafuckaboutsecurity -mem=AES256 `date +%Y%m%d`.full.7z /backup/*
-mv `date +%Y%m%d`.full.7z /home/someuser/
+7za a -y -tzip -p$ARCHIVEPASSWORD -mem=AES256 `date +%Y%m%d`.full.7z $ROOTFOLDER
+mv `date +%Y%m%d`.full.7z $ARCHIVEFOLDER
 
 #Storing old archive name
-datediff30 = `date -d "today - 30 days" +%Y%m%d`.full.7z
+datediff = `date -d "today - 30 days" +%Y%m%d`.full.7z
 
 #Sending / deleting archive to / from your awesome ftp storage
-curl -T /home/someuser/`date +%Y%m%d`.full.7z ftp://server --user user:pass
-curl --quote "-dele $datediff30" ftp://server --user user:pass
+curl -T $ARCHIVEFOLDER`date +%Y%m%d`.full.7z ftp://$FTPSERVER --user $FTPUSER:$FTPPASS
+curl --quote "-dele $datediff" ftp://$FTPSERVER --user $FTPUSER:$FTPPASS
 
 #Deleting all locales backup files
-rm /backup/WWW/*.tar.gz
-rm /backup/DBs/*.sql
-rm /home/someuser/*.7z
+rm $DBBACKUPDIR*.tar.gz
+rm $WWWBACKUPDIR*.sql
+rm $ARCHIVEFOLDER*.7z
